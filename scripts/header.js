2a82d7519c2d011d1e4340087069be056374c253
Edgar de Dios <eduardoddg60@gmail.com>
2019-07-16 00:58:31 -0500

    about me added

--- scripts/header.js
+++ scripts/header.js
@@ -1,15 +1,2 @@
-var header = document.getElementById('header');
-var headerBackground = document.getElementById('header-background');
-var menuMobile = document.getElementById('menu-mobile');
 
-var headerScroll = function headerScroll() {
-    if (posScroll > 150) {
-        header.style.top = "5px";
-        headerBackground.style.top = "0%";
-        menuMobile.style.top = "80px";
-    } else {
-        header.style.top = "12%";
-        headerBackground.style.top = "-100%";
-        menuMobile.style.top = "25%";
-    }
-}
+
